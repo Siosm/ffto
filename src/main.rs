b8e092333eac0a955fd6337c33862621baa3213b
Timothée Ravier <tim@siosm.fr>
2014-12-12 12:08:46 +0100

    General update and code cleanup

--- src/main.rs
+++ src/main.rs
@@ -4,72 +4,84 @@ extern crate url;
 
 use std::io::net::tcp::{TcpListener,TcpStream};
 use std::io::{Acceptor,Listener,Command};
+use std::io::process::ProcessExit;
 use url::Url;
 
-// Note: Error handling could be improved
 fn main() {
     let browser_command = "firefox";
     let address = "127.0.0.1:7777";
 
-    // Prepare a socket listening on localhost:7777
-    let listener = TcpListener::bind(address).unwrap();
-    let mut acceptor = listener.listen().unwrap();
+    let listener = match TcpListener::bind(address) {
+        Ok(l)  => l,
+        Err(e) => panic!("Could not bind to {}: {}", address, e)
+    };
+    let mut acceptor = match listener.listen() {
+        Ok(a)  => a,
+        Err(e) => panic!("Could not listen for connections: {}", e)
+    };
 
-    // Infinite loop to keep handling new connections.
     loop {
-        let tcp_stream = acceptor.accept().unwrap();
-        debug!("Accepted new connection");
-        spawn(proc() {
-            handle_client(tcp_stream, browser_command)
-        });
-    }
-}
-
-// Accept a new connection and listen for URLs
-fn handle_client(stream: TcpStream, browser_command: &'static str) {
-    debug!("Spawned new task");
-
-    let mut tcp_stream = stream;
-
-    // Note that as soon as read_to_str() returns, everything sent
-    // to the socket after this point will be discarded as once
-    // we're done working with the content we've just read, the
-    // tcpStream will be freed.
-    let message = tcp_stream.read_to_string().unwrap();
-
-    // Iterate over the lines in the received message
-    for line in message.as_slice().split('\n') {
-        debug!("Current line is: {}", line);
-
-        // This tries to convert the line to a Url struct
-        let url = Url::parse(line);
-
-        // On failure it returns None; on success a valid URL
-        match url {
-            Err(e) => { info!("No Url found: {}", e) }
-            Ok(u)  => {
-                debug!("Found Url in: {}", line);
-                if check_url(&u) {
-                    spawn_process(&u, browser_command)
-                }
-            }
+        match acceptor.accept() {
+            Ok(s)  => {
+                debug!("Accepted new incoming connection");
+                spawn(proc() {
+                    debug!("Spawned new task");
+                    handle_client(s, browser_command)
+                });
+            },
+            Err(e) => panic!("Could not handle incoming connection: {}", e)
         }
     }
 }
 
-// Check that the URL is actually usable
-fn check_url(u: &Url) -> bool {
+fn handle_client(stream: TcpStream, browser_command: &str) {
+    let mut tcp_stream = stream;
+
+    // FIXME: do we need to read in a loop?
+    let message = match tcp_stream.read_to_string() {
+        Ok(s)  => s,
+        Err(e) => panic!("Input isn't a valid UTF-8 string: {}", e)
+    };
+
+    for line in message.split('\n') {
+        debug!("Current line is: {}", line);
+
+        match Url::parse(line) {
+            Ok(u)  => {
+                debug!("Found URL in: {}", line);
+                if url_valid(&u) {
+                    debug!("Found URL is valid");
+                    spawn_browser(browser_command, format!("{}", u).as_slice());
+                }
+            },
+            Err(e) => info!("No URL found in '{}': {}", line, e)
+        }
+    }
+}
+
+fn url_valid(u: &Url) -> bool {
     (u.scheme.as_slice() == "http" || u.scheme.as_slice() == "https")
         && u.host().is_some()
 }
 
-// Spawn a browser to access the URL
-fn spawn_process(u: &Url, command: &'static str) {
-    debug!("Spawning process: {} {}", command, u);
-    let url = format!("{}", u);
+
+fn spawn_browser(command: &str, url: &str) {
+    debug!("Spawning process: {} {}", command, url);
     let mut child = match Command::new(command).arg(url).spawn() {
         Ok(child) => child,
-        Err(e)    => panic!("Failed to spawn process: {}. {}", command, e),
+        Err(e)    => panic!("Failed to spawn process '{}': {}", command, e)
     };
-    child.wait().unwrap().success();
+    match child.wait() {
+        Ok(p)  => {
+            if p.success() {
+                debug!("Process exited successfully");
+            } else {
+                match p {
+                    ProcessExit::ExitStatus(s) => panic!("Process exited with status: {}", s),
+                    ProcessExit::ExitSignal(s) => panic!("Process received signal: {}", s)
+                }
+            }
+        },
+        Err(e) => panic!("This should never happen! ({})", e)
+    }
 }
